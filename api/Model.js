f018f0e9c44a399d8242c242478ec3f4f98ea7b6
lollp18 <lorenzo123696@gmail.com>
2025-07-26 17:59:14 +0200

    ok

--- api/Model.js
+++ api/Model.js
@@ -38,7 +38,7 @@ class Model {
         "http://localhost:8080/",
         "https://csvdb-3.vercel.app/",
       "https://csv3.netlify.app/"],
-      credentials: 'include',
+      credentials: true,
       optionSuccessStatus: 200,
       exposedHeaders: ["set-cookie"],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'] 

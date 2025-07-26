8bd2340b33f46dc1e2ff34dd2d1d1ce8c66dedc6
lollp18 <lorenzo123696@gmail.com>
2025-07-26 16:59:47 +0200

    ok

--- api/Model.js
+++ api/Model.js
@@ -33,7 +33,7 @@ class Model {
 
     this.UserModel = mongoose.model("User", this.UserSchema)
     this.CorsOptions = {
-      origin:["http://localhost:3000", "http://localhost:8080"],
+      origin: ["http://localhost:3000", "http://localhost:8080","https://csvdb-3.vercel.app/"],
       credentials: true,
       optionSuccessStatus: 200,
       exposedHeaders: ["set-cookie"],

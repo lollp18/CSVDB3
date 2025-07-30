1f6da3c0394b43db9e8985581b7ba17d6bb8a377
lollp18 <lorenzo123696@gmail.com>
2025-07-30 12:01:58 +0200

    o

--- api/Control.js
+++ api/Control.js
@@ -33,11 +33,22 @@ class Controller extends Model {
   InitUse() {
 this.App.use((req, res, next) => {
 
+    
+
+    
+     res.set({
+            'Access-Control-Allow-Origin': 'true', 
+            'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token, Authorization', 
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Max-Age': '86400' 
+        });
+
 
  
     
 
-   
+    next(); 
 });
 
 

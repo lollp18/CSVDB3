66ecc0ffde86cd7145c8b61f5631fa6b9de95ac3
lollp18 <lorenzo123696@gmail.com>
2025-08-03 10:57:16 +0200

    ok

--- api/Control.js
+++ api/Control.js
@@ -134,7 +134,7 @@ class Controller extends Model {
       await User.save()
 
       res.cookie("CSVDB-AUTH", User.Authentication.SessionToken, {
-        
+        httpOnly: true,
         path: "/",
         sameSite: "none",
         secure: true,

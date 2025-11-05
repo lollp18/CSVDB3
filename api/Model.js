96007353c61e945f85d6d33597dc433ca28ef8bc
lorenzo123696 <lorenzo123696@gmail.com>
2025-11-05 13:03:21 +0000

    ok

--- api/Model.js
+++ api/Model.js
@@ -91,7 +91,7 @@ class Model {
     try {
       const SessionToken = req.cookies["CSVDB-AUTH"]
 
-      if (!SessionToken) return res.status(403).send("SessionToken")
+      if (!SessionToken) return res.status(403).send(JSON.stringify(SessionToken))
 
 
       const ExistingUser = await this.GetUserBySessionToken(SessionToken)

0e3beb6534b90037a33d1410d4b9fda06a2e7053
lorenzo123696 <lorenzo123696@gmail.com>
2025-11-07 09:56:42 +0000

    ok

--- api/Control.js
+++ api/Control.js
@@ -21,7 +21,7 @@ class Controller extends Model {
 
     this.CorsOptions = {
       origin:[
-    "http://localhost:3000",
+    
     "https://csv3.netlify.app",
     "https://3000-firebase-csv3-1762077727673.cluster-64pjnskmlbaxowh5lzq6i7v4ra.cloudworkstations.dev/",
     
@@ -49,7 +49,7 @@ class Controller extends Model {
   InitUse() {
 
     this.App.use(cors(this.CorsOptions))
-    this.App.options('*', cors(this.CorsOptions)); 
+    this.App.options('*', cors()); 
     this.App.use(express.json())
     this.App.use(cookieParser())
     this.App.use(compression())
@@ -148,6 +148,7 @@ class Controller extends Model {
         sameSite: "none",
         secure: true,
         maxAge: 900000,
+        httpOnly: true,
       })
 
       return res.status(201).json(User).end()
